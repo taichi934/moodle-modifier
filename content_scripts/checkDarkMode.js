f7deba9911b27ceda19fbde467b70cfb798b2209
taichi934 <banzai_sub_0z@icloud.com>
2022-09-25 14:02:07 +0000

    turn Dark mode on/off according to switch in popup

--- content_scripts/checkDarkMode.js
+++ content_scripts/checkDarkMode.js
@@ -1,12 +1,33 @@
-(()=>{
-    const isDarkMode = chrome.storage.sync.get(["isDarkMode"]);
-    const tabId = getTabId();
-    if (isDarkMode === true) {
-        chrome.scripting.insertCSS(
-            {
-                target: {tabId: tabId},
-                files: ["../css/darMode.css"],
-            }
-        );
+(() => {
+  const toggleDarkMode = (isDarkMode) => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
+    // document.documentElement.setAttribute('data-theme', storedTheme)
+  };
+
+  let isDarkMode;
+  chrome.storage.local.get(["isDarkMode"], (items) => {
+    isDarkMode = items.isDarkMode;
+    if (isDarkMode) {
+      toggleDarkMode(isDarkMode);
+    } else {
+      isDarkMode = false;
+    }
+  });
+
+  // popupでカラーテーマを変更したとき
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    isDarkMode = changes.isDarkMode;
+    console.log("updated isDarkMode => " + isDarkMode);
+    console.log(isDarkMode); // isDarkMode <= {newValue: true, oldValue: false}
+    toggleDarkMode(isDarkMode.newValue);
+  });
+
+  //   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  //     isDarkMode = request.isDarkMode;
+  //     toggleDarkMode(isDarkMode);
+  //   });
 })();
--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,42 @@
 const toggleDarkMode = (isDarkMode) => {
-    if (isDarkMode) {
-        document.documentElement.classList.add("dark");
-    } else {
-        document.documentElement.classList.remove("dark");
-    }
-    // document.documentElement.setAttribute('data-theme', storedTheme)
-}
+  if (isDarkMode) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+  // document.documentElement.setAttribute('data-theme', storedTheme)
+};
 
 let isDarkMode;
-chrome.storage.sync.get(["isDarkMode"], (result)=>{
-    // storageに値がなければundedinedを返す
-    if (isDarkMode == undefined) {
-        isDark = false;
-    }
-    isDarkMode = result.isDarkMode;
-    console.log("isDarkMode => " + isDarkMode);
+chrome.storage.local.get(["isDarkMode"], (result) => {
+  // storageに値がなければundedinedを返す
+  isDarkMode = result.isDarkMode;
+  if (isDarkMode == undefined) {
+    isDarkMode = false;
+  }
+  console.log("isDarkMode => " + isDarkMode);
 });
 
+// for popup/html
 toggleDarkMode(isDarkMode);
 
 const colorSwitch = document.getElementsByClassName("color-theme-switch")[0];
 
-colorSwitch.addEventListener("click", ()=>{
-    isDarkMode = !isDarkMode;
-    chrome.storage.sync.set({"isDarkMode": isDarkMode}, ()=>{
-        console.log("isDarkMode => " + isDarkMode);
-    });
+colorSwitch.addEventListener("click", () => {
+  isDarkMode = !isDarkMode;
+  chrome.storage.local.set({ isDarkMode: isDarkMode }, () => {
+    console.log("isDarkMode => " + isDarkMode);
     toggleDarkMode(isDarkMode);
+  });
+
+  // popupからcontent_scriptsにメッセージを送信 +++++++++++++++++++
+  // let tabId = await getTabId();
+  // chrome.tabs.sendMessage(
+  //     tabId: tabId,
+  //     message: {"isDarkMode": isDarkMode},
+  // );
+  // chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+  //     chrome.tabs.sendMessage(tabs[0].id, {"isDarkMode": isDarkMode},
+  //         console.log(response);
+  //     });
 });
