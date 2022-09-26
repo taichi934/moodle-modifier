89ab470769e649d34858c88e7ca0d78003812bf3
taichi934 <banzai_sub_0z@icloud.com>
2022-09-26 07:51:27 +0000

    fix bugs & change manifest

--- content_scripts/checkDarkMode.js
+++ content_scripts/checkDarkMode.js
@@ -1,33 +1,26 @@
 (() => {
   const toggleDarkMode = (isDarkMode) => {
     if (isDarkMode) {
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add('dark');
     } else {
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove('dark');
     }
-    // document.documentElement.setAttribute('data-theme', storedTheme)
   };
 
   let isDarkMode;
-  chrome.storage.local.get(["isDarkMode"], (items) => {
+  chrome.storage.local.get(['isDarkMode'], (items) => {
     isDarkMode = items.isDarkMode;
-    if (isDarkMode) {
-      toggleDarkMode(isDarkMode);
-    } else {
+    if (isDarkMode === undefined) {
       isDarkMode = false;
     }
+    toggleDarkMode(isDarkMode);
   });
 
   // popupでカラーテーマを変更したとき
   chrome.storage.onChanged.addListener((changes, namespace) => {
     isDarkMode = changes.isDarkMode;
-    console.log("updated isDarkMode => " + isDarkMode);
+    console.log('updated isDarkMode => ' + isDarkMode);
     console.log(isDarkMode); // isDarkMode <= {newValue: true, oldValue: false}
     toggleDarkMode(isDarkMode.newValue);
   });
-
-  //   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  //     isDarkMode = request.isDarkMode;
-  //     toggleDarkMode(isDarkMode);
-  //   });
 })();
--- content_scripts/loginByCollegeID.js
+++ content_scripts/loginByCollegeID.js
@@ -1,8 +1,27 @@
 (() => {
-  // login by <your college> id
-  const a = document.getElementsByClassName(
-    "btn btn-lg btn-block btn-warning"
-  )[0];
-  const url = a.href;
-  location.assign(url);
+  const toggleDarkMode = (isDarkMode) => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  };
+
+  let isDarkMode;
+  chrome.storage.local.get(['isDarkMode'], (items) => {
+    isDarkMode = items.isDarkMode;
+    if (isDarkMode === undefined) {
+      isDarkMode = false;
+    }
+    toggleDarkMode(isDarkMode);
+  });
+
+  window.addEventListener('load', () => {
+    // login by <your college> id
+    const a = document.getElementsByClassName(
+      'btn btn-lg btn-block btn-warning'
+    )[0];
+    const url = a.href;
+    location.assign(url);
+  });
 })();
