91472b26720fc5c90fd660330ae4008fbab3ee33
taichi934 <banzai_sub_0z@icloud.com>
2022-09-26 15:39:03 +0000

    separete toggle dark mode functions to anotherfile

--- content_scripts/loginByCollegeID.js
+++ content_scripts/loginByCollegeID.js
@@ -1,21 +1,4 @@
 (() => {
-  const toggleDarkMode = (isDarkMode) => {
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  };
-
-  let isDarkMode;
-  chrome.storage.local.get(['isDarkMode'], (items) => {
-    isDarkMode = items.isDarkMode;
-    if (isDarkMode === undefined) {
-      isDarkMode = false;
-    }
-    toggleDarkMode(isDarkMode);
-  });
-
   window.addEventListener('load', () => {
     // login by <your college> id
     const a = document.getElementsByClassName(
