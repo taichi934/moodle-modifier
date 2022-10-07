64f1b384e1feebd7277d31a7fb98bdd4d5c3a6cb
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-10-08 02:24:08 +0900

    refactoring

--- content_scripts/addCalendar.js
+++ content_scripts/addCalendar.js
@@ -1,33 +1,26 @@
 (() => {
   const main = () => {
+    // iframeでカレンダーページを読み取り
     const calendarPage = document.createElement("iframe");
     calendarPage.src = "./calendar/view.php?view=month";
-    // calendarPage.style.display = "none";
-    // document.getElementById("page-wrapper").appendChild(calendarPage);
+    calendarPage.style.display = "none";
     document.body.appendChild(calendarPage);
 
-    // console.log(calendarPage.contentDocument.document);
+    // カレンダーを包む用のdiv
+    const calendarDiv = document.createElement("div");
+    calendarDiv.classList.add("calendar-table", "block", "card", "mb-3");
 
     calendarPage.addEventListener("load", () => {
-      console.log("hello from onLoad");
-      console.log(calendarPage);
-      console.log("contentWindow: " + calendarPage.contentWindow.document);
-      console.log("contentDocument: " + calendarPage.contentDocument);
-
-      const tableDiv = document.createElement("div");
-      const calendar = calendarPage.contentWindow.document.querySelector(
+      // iframeからカレンダーを取得
+      const calendar = calendarPage.contentDocument.querySelector(
         "div.calendarwrapper"
       );
-      tableDiv.classList.add("calendar-table", "block", "card", "mb-3");
-      tableDiv.appendChild(calendar);
-      console.log("calendar: " + calendar);
-      // document.getElementById("page-wrapper").appendChild(calendar);
+      calendarDiv.appendChild(calendar); // カレンダーをdivで包む
 
       const navRight = document.getElementById("block-region-side-pre");
       let links = navRight.children[2];
-      navRight.insertBefore(tableDiv, links);
+      navRight.insertBefore(calendarDiv, links);
 
-      // document.getElementById("page-wrapper").removeChild(calendarPage);
       document.body.removeChild(calendarPage);
     });
   };
@@ -41,24 +34,3 @@
     }
   });
 })();
-// トップページ：   https://moodle2022.wakayama-u.ac.jp/2022/
-// カレンダーページ：https://moodle2022.wakayama-u.ac.jp/2022/calendar/view.php?view=month
-
-/* <script type="text/javascript">
-
-var iframe = document.createElement("iframe");
-iframe.src = "./ex1.html";
-iframe.style.display = "none";
-document.body.appendChild( iframe );
-
-iframe.onload = function(){
-
-var a = iframe.contentWindow.document.getElementsByTagName("a")[ 0 ];
-
-document.getElementsByTagName("h1")[ 0 ].innerHTML = a.outerHTML;
-
-document.body.removeChild( iframe );
-
-};
-
-</script> */
