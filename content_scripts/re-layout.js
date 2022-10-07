f534e8f51f1f2d211d397935e9986edbd98b359c
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-10-07 17:19:01 +0900

    remove redundant console.log

--- content_scripts/re-layout.js
+++ content_scripts/re-layout.js
@@ -1,8 +1,8 @@
 // 無名関数を定義して呼び出すのが定例っぽい
 (() => {
-  window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener("DOMContentLoaded", () => {
     // 自動ログイン
-    const loginLink = document.querySelector('.login');
+    const loginLink = document.querySelector(".login");
     // ログインのリンクがある場合
     if (loginLink != null) {
       //click login link
@@ -16,12 +16,11 @@
     const parentDiv = document.querySelector("div[role = 'main']");
     parentDiv.insertBefore(parentDiv.children[5], parentDiv.children[1]);
     parentDiv.insertBefore(parentDiv.children[2], parentDiv.children[6]);
-    console.log(parentDiv.children);
 
     // header画像を右のナビゲーションの最上部に表示
-    const navRight = document.getElementById('block-region-side-pre');
-    const header = document.getElementsByClassName('card')[0];
-    header.style.marginBottom = '1rem';
+    const navRight = document.getElementById("block-region-side-pre");
+    const header = document.getElementsByClassName("card")[0];
+    header.style.marginBottom = "1rem";
     navRight.insertBefore(header, navRight.children[1]);
 
     // リンク集を上に
@@ -30,7 +29,7 @@
     navRight.insertBefore(links, myCourses);
 
     // リンク集の中に図書館OPACへのリンクを教育サポートシステムの下に配置
-    const opacLink = document.createElement('p');
+    const opacLink = document.createElement("p");
     opacLink.innerHTML = `<a href="https://opac.center.wakayama-u.ac.jp/opac/opac_search/?lang=0" target="_blank">和歌山大学図書館OPAC</a>`;
 
     // リンク集が余分なdivに囲まれてるから取り除く
