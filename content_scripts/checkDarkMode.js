6cb26dc6d280d7fbd4b3b4f83acabffda4e8438f
taichi934 <banzai_sub_0z@icloud.com>
2022-10-18 08:21:24 +0000

    ignore not-isdarkmode changes

--- content_scripts/checkDarkMode.js
+++ content_scripts/checkDarkMode.js
@@ -1,25 +1,29 @@
 (() => {
-  const toggleDarkMode = (isDarkMode) => {
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  };
+    let isDarkMode;
+    chrome.storage.sync.get(['isDarkMode'], (items) => {
+        isDarkMode = items.isDarkMode;
+        if (isDarkMode === undefined) {
+            isDarkMode = false;
+        }
+        toggleDarkMode(isDarkMode);
+    });
 
-  let isDarkMode;
-  chrome.storage.sync.get(['isDarkMode'], (items) => {
-    isDarkMode = items.isDarkMode;
-    if (isDarkMode === undefined) {
-      isDarkMode = false;
-    }
-    toggleDarkMode(isDarkMode);
-  });
-
-  // popupでカラーテーマを変更したとき
-  chrome.storage.onChanged.addListener((changes) => {
-    isDarkMode = changes.isDarkMode;
-    // isDarkMode <= {newValue: true, oldValue: false}
-    toggleDarkMode(isDarkMode.newValue);
-  });
+    // popupでカラーテーマを変更したとき
+    chrome.storage.onChanged.addListener((changes) => {
+        if (changes.isDarkMode === undefined) return;
+        isDarkMode = changes.isDarkMode;
+        console.log(
+            'read storage at checkdarkMode.js -> ' + changes.isDarkMode
+        );
+        // isDarkMode <= {newValue: true, oldValue: false}
+        toggleDarkMode(isDarkMode.newValue);
+    });
 })();
+
+function toggleDarkMode(isDarkMode) {
+    if (isDarkMode) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+}
