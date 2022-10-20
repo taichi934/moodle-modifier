defa15e4144f85545e1b54575dc16b52f4e69117
taichi934 <banzai_sub_0z@icloud.com>
2022-10-20 16:59:39 +0000

    add func to count at every textarea(iframe)

--- content_scripts/wordCount.js
+++ content_scripts/wordCount.js
@@ -1,8 +1,25 @@
 (() => {
   //   <textarea id="area">文字数 : 0 文字</textarea>
   //   <p id="inputlength1">0文字</p>
-  document.getElementById('area').onkeydown = () => {
-    document.getElementById('inputlength').innerHTML =
-      '文字数 : ' + str.length + ' 文字';
-  };
+  // document.getElementById("area").onkeydown = () => {
+  //   document.getElementById("inputlength").innerHTML =
+  //     "文字数 : " + str.length + " 文字";
+  // };
+
+  let iframeArray = Array.from(document.getElementsByTagName("iframe"));
+  iframeArray.pop();
+
+  for (const iframe of iframeArray) {
+    const pArray = iframe.contentDocument.getElementsByTagName("p");
+    let count = 0;
+    for (const p of pArray) {
+      if (p.firstChild.length) count += p.firstChild.length;
+    }
+    console.log(count);
+  }
 })();
+
+// 複数iframeがあり、最後のiframeは拡張機能が挿入したもの
+// let iframe = document.getElementsByTagName("iframe")[0]
+// iframe.contentDocument.getElementsByTagName("p")
+// iframe.contentDocument.getElementsByTagName("p")[0].firstChild.length;
