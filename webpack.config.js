f8c0edd7c985bd5494c259de1ab2ae70811f10c7
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-10-25 20:59:00 +0900

    add configs for typescript

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: 'development',
+
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: {
+        main: path.resolve(__dirname, './content_scripts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader',
+            },
+        ],
+    },
+    // import 文で .ts ファイルを解決するため
+    // これを定義しないと import 文で拡張子を書く必要が生まれる。
+    // フロントエンドの開発では拡張子を省略することが多いので、
+    // 記載したほうがトラブルに巻き込まれにくい。
+    resolve: {
+        // 拡張子を配列で指定
+        extensions: ['.ts', '.js'],
+    },
+};
