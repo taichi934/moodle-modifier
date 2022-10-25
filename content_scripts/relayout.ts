47a7d11633d04718c820f014b8292a9386cc339c
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-10-25 23:07:46 +0900

    refactor type anotation

--- content_scripts/relayout.ts
+++ content_scripts/relayout.ts
@@ -2,35 +2,36 @@
 (() => {
     window.addEventListener('DOMContentLoaded', () => {
         // 自動ログイン
-        const loginLink =
-            document.querySelector<HTMLAnchorElement>('.login > a');
+        const loginLink = document.querySelector(
+            '.login > a'
+        ) as HTMLLinkElement;
         // ログインのリンクがある場合
-        if (loginLink != null) {
-            //click login link
-            const hrefUrl: any = loginLink?.href;
-            location.assign(hrefUrl);
+        //click login link
+        const hrefUrl = loginLink.href;
+        location.assign(hrefUrl);
 
-            // login process continues on loginByCollegeID.js
-        }
+        // login process continues on loginByCollegeID.js
 
         // コースを上に表示
-        const parentDiv =
-            document.querySelector<HTMLElement>("div[role = 'main']");
-        if (parentDiv != null) {
-            parentDiv.insertBefore(
-                parentDiv.children[5] as Node,
-                parentDiv.children[1] as Node
-            );
-            parentDiv.insertBefore(
-                parentDiv.children[2] as Node,
-                parentDiv.children[6] as Node
-            );
-        }
+        const parentDiv = document.querySelector(
+            "div[role = 'main']"
+        ) as HTMLDivElement;
+        parentDiv.insertBefore(
+            parentDiv.children[5] as Node,
+            parentDiv.children[1] as Node
+        );
+        parentDiv.insertBefore(
+            parentDiv.children[2] as Node,
+            parentDiv.children[6] as Node
+        );
 
         // header画像を右のナビゲーションの最上部に表示
-        const navRight = document.getElementById('block-region-side-pre');
-        if (navRight === null) return;
-        const header: any = document.getElementsByClassName('card')[0];
+        const navRight = document.getElementById(
+            'block-region-side-pre'
+        ) as HTMLElement;
+        const header = document.getElementsByClassName(
+            'card'
+        )[0] as HTMLDivElement;
         header.style.marginBottom = '1rem';
         navRight.insertBefore(header, navRight.children[1] as Node);
 
@@ -40,7 +41,7 @@
         navRight.insertBefore(links as Node, myCourses as Node);
 
         // リンク集の中に図書館OPACへのリンクを教育サポートシステムの下に配置
-        const opacLink = document.createElement('p');
+        const opacLink: HTMLParagraphElement = document.createElement('p');
         opacLink.innerHTML = `<a href="https://opac.center.wakayama-u.ac.jp/opac/opac_search/?lang=0" target="_blank">和歌山大学図書館OPAC</a>`;
 
         // リンク集が余分なdivに囲まれてるから取り除く
