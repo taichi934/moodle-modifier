54c70be0e83ddaaace6ff0db814bdbe66754d94c
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-11-02 13:23:20 +0900

    remove initial logo toggling based on color mode

--- content_scripts/switchLogo.js
+++ content_scripts/switchLogo.js
@@ -1,32 +1,22 @@
 (() => {
     window.addEventListener('DOMContentLoaded', () => {
-        // Change wadai logo image on top of right nav
-        const changeLogo = (isDarkMode) => {
-            const img = document.querySelector('.logo img');
-            if (isDarkMode) {
-                img.src = chrome.runtime.getURL(
-                    'images/wadai-logo-trans-complete.png'
-                );
-            } else {
-                img.src = chrome.runtime.getURL('images/wadai-logo.png');
-            }
-        };
-
         let isDarkMode;
-        chrome.storage.sync.get(['isDarkMode'], (items) => {
-            isDarkMode = items.isDarkMode;
-            if (isDarkMode === undefined) {
-                isDarkMode = false;
-            }
-            changeLogo(isDarkMode);
-        });
 
         // popupでカラーテーマを変更したとき
         chrome.storage.onChanged.addListener((changes) => {
             if (changes.isDarkMode === undefined) return;
-
             isDarkMode = changes.isDarkMode;
             changeLogo(isDarkMode.newValue);
         });
     });
 })();
+
+// Change wadai logo image on top of right nav
+function changeLogo(isDarkMode) {
+    const img = document.querySelector('.logo .img-fluid');
+    if (isDarkMode) {
+        img.src = chrome.runtime.getURL('images/wadai-logo-trans-complete.png');
+    } else {
+        img.src = chrome.runtime.getURL('images/wadai-logo.png');
+    }
+}
