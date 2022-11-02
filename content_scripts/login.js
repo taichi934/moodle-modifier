5f49c82f34f26249dc5280aee5bf252657475a87
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-11-02 13:17:30 +0900

    hide all elements while login process

---
+++ content_scripts/login.js
@@ -0,0 +1,25 @@
+(() => {
+    window.addEventListener('DOMContentLoaded', () => {
+        // if you login for the first time, you need to login by <your college> id manually
+
+        // in other cases, you just push a login button
+        // this code just do that automatically
+        const a = document.getElementsByClassName(
+            'btn btn-lg btn-block btn-warning'
+        )[0];
+        const url = a.href;
+
+        // 要素を全て非表示にする（ボタンとかがチラつくよりもマシ）
+        everythingHidden();
+
+        location.assign(url);
+    });
+})();
+
+function everythingHidden() {
+    const body = document.querySelector('body');
+    for (const el of Array.from(body.children)) {
+        if ('script' in el) continue;
+        el.style.display = 'none';
+    }
+}
