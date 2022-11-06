d6492da2f279b0dc0f068f050b13ba3491b2f5fa
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-11-06 17:19:34 +0900

    change directory tree #51

---
+++ src/popup/popup.js
@@ -0,0 +1,34 @@
+const toggleDarkMode = (isDarkMode, colorSwitch, modeText) => {
+  if (isDarkMode) {
+    document.documentElement.classList.add('dark');
+    colorSwitch.checked = true;
+    modeText.textContent = 'ダークモード🌓';
+  } else {
+    document.documentElement.classList.remove('dark');
+    colorSwitch.checked = false;
+    modeText.textContent = 'ライトモード🌓';
+  }
+};
+
+let isDarkMode;
+const modeText = document.getElementById('mode-text');
+
+chrome.storage.sync.get(['isDarkMode'], (result) => {
+  // storageに値がなければundedinedを返す
+  if (result.isDarkMode === undefined) {
+    isDarkMode = false;
+  } else {
+    isDarkMode = result.isDarkMode;
+  }
+  toggleDarkMode(isDarkMode, colorSwitch, modeText); // for switch in popup/html
+});
+
+// const colorSwitch = document.getElementsByClassName('switch')[0];
+const colorSwitch = document.querySelector(`input[type = 'checkbox']`);
+
+colorSwitch.addEventListener('change', () => {
+  isDarkMode = !isDarkMode;
+  chrome.storage.sync.set({ isDarkMode: isDarkMode }, () => {
+    toggleDarkMode(isDarkMode, colorSwitch, modeText); // for switch in popup/html
+  });
+});
