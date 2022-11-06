0be18806968ff50f9f83ff6d86f313056390c762
Taichi Ogi <timeismoney529@i.softbank.jp>
2022-11-06 17:22:45 +0900

    zip files needed for uploading #51

---
+++ zip.js
@@ -0,0 +1,16 @@
+const zip = require('bestzip');
+let destination = `./release/moodle_modifier`;
+if (process.argv.length >= 3) {
+    destination += `_${process.argv[2]}`;
+}
+destination += '.zip';
+zip({
+    source: [
+        'manifest.json',
+        'dist/*',
+        'src/css/*',
+        'src/images/*',
+        'src/popup/*',
+    ],
+    destination,
+});
